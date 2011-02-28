220ed4ea7f61b9fbdec8c74684bbeb61b95ab01f
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2011-02-28 21:03:11 +0530

    [Prasanna] Added Dynamic generationof TextViews

--- src/com/prasans/activity/EnterAnswerActivity.java
+++ src/com/prasans/activity/EnterAnswerActivity.java
@@ -2,27 +2,82 @@ package com.prasans.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
-import com.prasans.R;
 
+import static android.view.ViewGroup.LayoutParams.FILL_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static android.widget.LinearLayout.VERTICAL;
 import static com.prasans.utils.AppConstants.COUNT;
 import static com.prasans.utils.AppConstants.TEST_NAME;
-import static java.lang.String.valueOf;
 
 public class EnterAnswerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.enter_answers);
         Bundle bundle = getIntent().getExtras();
-        setTextTo(R.id.testNameDisplay, bundle.getString(TEST_NAME));
-        setTextTo(R.id.quesCountDisplay, valueOf(bundle.getInt(COUNT)));
+        LinearLayout linearLayout = new LinearLayout(this);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(FILL_PARENT, WRAP_CONTENT);
+        linearLayout.setLayoutParams(params);
+
+        linearLayout.setOrientation(VERTICAL);
+        linearLayout.addView(textView("Enter the Answers for the Questions"));
+        linearLayout.addView(textView("Test Name :" + bundle.getString(TEST_NAME)));
+        int count = bundle.getInt(COUNT);
+        linearLayout.addView(textView("No. Of Questions: " + count));
+        linearLayout.addView(tableLayout(count));
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.addView(linearLayout);
+        setContentView(scrollView);
     }
 
-    private void setTextTo(int textField, String testName) {
-        TextView textView = (TextView) findViewById(textField);
-        textView.setText(testName);
+    private TableLayout tableLayout(int count) {
+        TableLayout tableLayout = new TableLayout(this);
+        tableLayout.setStretchAllColumns(true);
+        int noOfRows = count / 5;
+        for (int i = 0; i < noOfRows; i++) {
+            int rowId = 5 * i;
+            tableLayout.addView(createOneFullRow(rowId));
+        }
+        int individualCells = count % 5;
+        tableLayout.addView(createLeftOverCells(individualCells, count));
+        return tableLayout;
     }
 
+    private TableRow createLeftOverCells(int individualCells, int count) {
+        TableRow tableRow = new TableRow(this);
+        tableRow.setPadding(0,10,0,0);
+        int rowId = count - individualCells;
+        for (int i = 1; i <= individualCells; i++) {
+            tableRow.addView(editText(String.valueOf(rowId + i)));
+        }
+        return tableRow;
+    }
+
+    private TableRow createOneFullRow(int rowId) {
+        TableRow tableRow = new TableRow(this);
+        tableRow.setPadding(0,10,0,0);
+        for (int i = 1; i <= 5; i++) {
+            tableRow.addView(editText(String.valueOf(rowId + i)));
+        }
+        return tableRow;
+    }
+
+    private TextView textView(String text) {
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        return textView;
+    }
+
+    private EditText editText(String hint) {
+        EditText editText = new EditText(this);
+        editText.setHint(hint);
+        return editText;
+    }
 }
