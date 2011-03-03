65da1b44de24c9b1c0cfd53ffcdb46acab9359a0
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2011-03-03 17:54:49 +0530

    Wired SMSMonitor with SendSMS

--- src/com/prasans/activity/SMSMonitor.java
+++ src/com/prasans/activity/SMSMonitor.java
@@ -3,6 +3,7 @@ package com.prasans.activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
@@ -17,13 +18,21 @@ public class SMSMonitor extends BroadcastReceiver {
             Object[] pduArray = (Object[]) intent.getExtras().get("pdus");
             SmsMessage[] messages = new SmsMessage[pduArray.length];
 
+            Bundle bundle = new Bundle();
             for (int i = 0; i < pduArray.length; i++) {
                 messages[i] = SmsMessage.createFromPdu((byte[]) pduArray[i]);
                 Toast.makeText(context, "You have received: " + messages.length +
-                        "SMS from " +messages[i].getDisplayOriginatingAddress()
-                        ,Toast.LENGTH_LONG).show();
+                        "SMS from " + messages[i].getDisplayOriginatingAddress()
+                        , Toast.LENGTH_LONG).show();
+                bundle.putString("phoneNumber",messages[i].getDisplayOriginatingAddress());
             }
             Log.d("MySMSMonitor", "SMS Message Received.");
+
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.putExtras(bundle);
+            callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            callIntent.setClass(context, SendSMS.class);
+            context.startActivity(callIntent);
         }
     }
 }
