6cc8a813d233da9b11b2e446144f4bca5d8d06ef
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2011-03-06 09:30:40 +0530

    Renamed SMSReceivedActivity

--- src/com/prasans/activity/EvaluateReceivedText.java
+++ src/com/prasans/activity/EvaluateReceivedText.java
@@ -3,11 +3,9 @@ package com.prasans.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ReceiveSMS extends Activity {
+public class EvaluateReceivedText extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        
     }
 }
