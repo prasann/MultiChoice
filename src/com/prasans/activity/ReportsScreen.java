abbd3adc2375c497d66587d3e2b49d7a884e9a6b
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2011-03-27 10:06:03 +0530

    Completed Reports Screen Listing Section

--- src/com/prasans/activity/ReportsScreen.java
+++ src/com/prasans/activity/ReportsScreen.java
@@ -3,28 +3,50 @@ package com.prasans.activity;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.text.util.Linkify;
-import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.prasans.R;
 import com.prasans.adapter.TestInfoDB;
+import com.prasans.adapter.TestInfoListAdapter;
+import com.prasans.domain.TestInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportsScreen extends ListActivity {
 
     private TestInfoDB testInfoDB;
-    private SimpleCursorAdapter simpleCursorAdapter;
+    private TestInfoListAdapter testInfoAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list);
+        setContentView(R.layout.report_list);
         testInfoDB = new TestInfoDB(this);
-        Cursor cursor = testInfoDB.fetchAllTests();
-        startManagingCursor(cursor);
-        String[] cols = new String[]{TestInfoDB.TEST_NAME};
-        int[] names = new int[]{R.id.row_tv};
+        List<TestInfo> infoList = getTestInfo();
+        testInfoAdapter = new TestInfoListAdapter(this, R.layout.row_list, infoList);
+        this.setListAdapter(testInfoAdapter);
+    }
 
-        simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.report_list, cursor, cols, names);
-        this.setListAdapter(simpleCursorAdapter);
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        TestInfo testInfo = (TestInfo)this.getListAdapter().getItem(position);
+        Toast.makeText(this, "TestCode: "+testInfo.getTestCode(), Toast.LENGTH_LONG).show();
+    }
+
+    private List<TestInfo> getTestInfo() {
+        List<TestInfo> testInfoList = new ArrayList<TestInfo>();
+        Cursor cursor = testInfoDB.fetchAllTests();
+        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            TestInfo testInfo = new TestInfo();
+            testInfo.setTestCode(cursor.getString(cursor.getColumnIndex(TestInfoDB.TEST_CODE)));
+            testInfo.setTestName(cursor.getString(cursor.getColumnIndex(TestInfoDB.TEST_NAME)));
+            testInfo.setStatus(cursor.getInt(cursor.getColumnIndex(TestInfoDB.OPEN)));
+            testInfoList.add(testInfo);
+        }
+        return testInfoList;
     }
 }
 
---
+++ src/com/prasans/domain/TestInfo.java
@@ -0,0 +1,31 @@
+package com.prasans.domain;
+
+public class TestInfo {
+    private String testName;
+    private String testCode;
+    private int status;
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getTestCode() {
+        return testCode;
+    }
+
+    public void setTestCode(String testCode) {
+        this.testCode = testCode;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
