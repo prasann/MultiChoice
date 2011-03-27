884412dd69ba0762bec05c6d98458e397e974287
prasann <prasann@shadowfax.corporate.thoughtworks.com>
2011-03-27 08:29:34 +0530

    Added Background image for the application

--- src/com/prasans/activity/ReportsScreen.java
+++ src/com/prasans/activity/ReportsScreen.java
@@ -3,10 +3,11 @@ package com.prasans.activity;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import com.prasans.R;
 import com.prasans.adapter.TestInfoDB;
-
 public class ReportsScreen extends ListActivity {
 
     private TestInfoDB testInfoDB;
@@ -15,12 +16,15 @@ public class ReportsScreen extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
         testInfoDB = new TestInfoDB(this);
         Cursor cursor = testInfoDB.fetchAllTests();
         startManagingCursor(cursor);
         String[] cols = new String[]{TestInfoDB.TEST_NAME};
         int[] names = new int[]{R.id.row_tv};
+
         simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.report_list, cursor, cols, names);
         this.setListAdapter(simpleCursorAdapter);
     }
 }
+
